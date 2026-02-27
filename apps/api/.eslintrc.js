045aca76945635650cd61a2d243ab18c02b0e3aa
nhenan <henan.noel@ynov.com>
2026-02-27 15:14:37 +0100

    fix(eslint): adjust rules to fix linting errors

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -10,5 +10,6 @@ module.exports = {
     '@typescript-eslint/no-unsafe-assignment': 'warn',
     '@typescript-eslint/no-unsafe-call': 'warn',
     '@typescript-eslint/no-unsafe-member-access': 'warn',
+    '@typescript-eslint/no-unsafe-return': 'warn',
   },
 };
