556841fa5d51141849a2df04901b984bf924fd2c
nhenan <henan.noel@ynov.com>
2026-03-03 10:09:36 +0100

    feat(users): add resource users + CRUD and http request done

---
+++ apps/api/src/users/users.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User as UserModel } from 'generated/prisma';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll(): Promise<UserModel[]> {
+    return this.usersService.findAll({
+      orderBy: { id: 'asc' },
+    });
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<UserModel | null> {
+    return this.usersService.findOne({ id: Number(id) });
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<UserModel> {
+    return this.usersService.update({
+      where: { id: Number(id) },
+      data: updateUserDto,
+    });
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<UserModel> {
+    return this.usersService.remove({ id: Number(id) });
+  }
+}
