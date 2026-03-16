92c1c4b1ecf41f3a119d88e772bac126b6d405b7
nhenan <henan.noel@ynov.com>
2026-03-16 15:32:17 +0100

    Feat(auth): add validation of provider to log in with OAuth

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule,
     ConfigModule,
     JwtModule.registerAsync({
       inject: [ConfigService],
--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from 'prisma/prisma.service';
 import bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { Provider } from '../../prisma/generated/prisma/client';
+import { GoogleProfile } from './interfaces/google-profile.interface';
 
 @Injectable()
 export class AuthService {
@@ -12,7 +13,7 @@ export class AuthService {
   ) {}
 
   async hashPassword(password: string): Promise<string> {
-    const salt = await bcrypt.genSalt();
+    const salt = await bcrypt.genSalt(12);
     return bcrypt.hash(password, salt);
   }
 
@@ -42,5 +43,56 @@ export class AuthService {
 
     const token = await this.generateToken(user.id, user.email);
     return { token };
+  }
+
+  async validateOAuthLogin(profile: GoogleProfile) {
+    const email = profile.emails?.[0]?.value;
+    const providerId = profile.id;
+
+    if (!email) throw new Error('OAuth email missing');
+
+    const existingProvider = await this.prisma.authProvider.findUnique({
+      where: {
+        provider_provider_id: {
+          provider: Provider.google,
+          provider_id: providerId,
+        },
+      },
+      include: { user: true },
+    });
+
+    if (existingProvider) {
+      return {
+        token: await this.generateToken(
+          existingProvider.user.id,
+          existingProvider.user.email,
+        ),
+      };
+    }
+
+    let user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      user = await this.prisma.user.create({
+        data: {
+          email,
+          first_name: profile.name?.givenName,
+          last_name: profile.name?.familyName,
+          authProviders: {
+            create: { provider: Provider.google, provider_id: providerId },
+          },
+        },
+      });
+    } else {
+      await this.prisma.authProvider.create({
+        data: {
+          provider: Provider.google,
+          provider_id: providerId,
+          userId: user.id,
+        },
+      });
+    }
+
+    return { token: await this.generateToken(user.id, user.email) };
   }
 }
