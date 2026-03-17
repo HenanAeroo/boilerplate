d730cf787c3f1115f362df7f7782d3a9f71a71d0
nhenan <henan.noel@ynov.com>
2026-03-17 16:09:30 +0100

    Refactor(users): removed guard for now

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,19 +1,9 @@
-import {
-  Controller,
-  Get,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User as UserModel } from '../../prisma/generated/prisma/client';
-import { JwtAuthGuard } from 'auth/guards/jwt-auth.guard';
 
 @Controller('users')
-@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
