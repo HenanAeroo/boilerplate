d3e028bdb2d59edb46868c796eee9bf2768a6211
nhenan <henan.noel@ynov.com>
2026-04-08 09:42:31 +0200

    tests(api): fix jest config and add unit test mocks

--- apps/api/src/auth/auth.controller.spec.ts
+++ apps/api/src/auth/auth.controller.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PrismaService } from 'prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from 'users/users.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -8,7 +12,13 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: PrismaService, useValue: {} },
+        { provide: JwtService, useValue: {} },
+        { provide: ConfigService, useValue: {} },
+        { provide: UsersService, useValue: {} },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
